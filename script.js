48a5c7b5cb2801bd45c8bc55528f7dbdd8fd2966
Darya <darasechkina@gmail.com>
2024-10-21 23:21:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-window.onload = () => {
-    generateStars();
-};
+// window.onload = () => {
+//     generateStars();
+// };
 
-function generateStars() {
-    const starContainer = document.getElementById('star-container');
-    const starSound = document.getElementById('star-sound');
+// function generateStars() {
+//     const starContainer = document.getElementById('star-container');
+//     const starSound = document.getElementById('star-sound');
 
-    for (let i = 0; i < 100; i++) {
-        let star = document.createElement('div');
-        star.className = 'star';
-        let size = Math.random() * 20 + 10; // Задаем случайный размер звезды
-        star.style.width = `${size}px`;
-        star.style.height = `${size}px`;
-        star.style.borderBottomWidth = `${size * 1.7}px`;
+//     for (let i = 0; i < 100; i++) {
+//         let star = document.createElement('div');
+//         star.className = 'star';
+//         let size = Math.random() * 20 + 10; // Задаем случайный размер звезды
+//         star.style.width = `${size}px`;
+//         star.style.height = `${size}px`;
+//         star.style.borderBottomWidth = `${size * 1.7}px`;
 
-        star.style.top = `${Math.random() * 100}%`;
-        star.style.left = `${Math.random() * 100}%`;
-        star.style.borderBottomColor = getRandomStarColor();
+//         star.style.top = `${Math.random() * 100}%`;
+//         star.style.left = `${Math.random() * 100}%`;
+//         star.style.borderBottomColor = getRandomStarColor();
 
-        starContainer.appendChild(star);
+//         starContainer.appendChild(star);
 
-        star.addEventListener('click', () => {
-            starSound.play();
-            star.remove();
-        });
-    }
-}
+//         star.addEventListener('click', () => {
+//             starSound.play();
+//             star.remove();
+//         });
+//     }
+// }
 
-function getRandomStarColor() {
-    const colors = ['#fdfd96', '#ffffff', '#fffacd', '#ffffe0'];
-    return colors[Math.floor(Math.random() * colors.length)];
-}
+// function getRandomStarColor() {
+//     const colors = ['#fdfd96', '#ffffff', '#fffacd', '#ffffe0'];
+//     return colors[Math.floor(Math.random() * colors.length)];
+// }
