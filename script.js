0a514eac5f4839a7ce5c838661c69442cc1b58c9
Darya <darasechkina@gmail.com>
2024-10-21 22:55:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const starContainer = document.getElementById('star-container');
-const startButton = document.getElementById('start-game');
-
-startButton.addEventListener('click', () => {
-    starContainer.style.display = 'block';
+window.onload = () => {
     generateStars();
-});
+};
 
 function generateStars() {
-    starContainer.innerHTML = '';
-    for (let i = 0; i < 50; i++) {
+    const starContainer = document.getElementById('star-container');
+    const starSound = document.getElementById('star-sound');
+
+    for (let i = 0; i < 100; i++) {
         let star = document.createElement('div');
         star.className = 'star';
+        let size = Math.random() * 20 + 10; // Задаем случайный размер звезды
+        star.style.width = `${size}px`;
+        star.style.height = `${size}px`;
+        star.style.borderBottomWidth = `${size * 1.7}px`;
+
         star.style.top = `${Math.random() * 100}%`;
         star.style.left = `${Math.random() * 100}%`;
+        star.style.borderBottomColor = getRandomStarColor();
+
         starContainer.appendChild(star);
+
         star.addEventListener('click', () => {
+            starSound.play();
             star.remove();
         });
     }
+}
+
+function getRandomStarColor() {
+    const colors = ['#fdfd96', '#ffffff', '#fffacd', '#ffffe0'];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
